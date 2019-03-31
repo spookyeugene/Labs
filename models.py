010267cc534ff5c09ca2d1fa6f5a653e4d9cc90b
spookyeugene <48047078+spookyeugene@users.noreply.github.com>
2019-03-31 19:02:07 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ class Post(db.Model):
 
     def __init__(self, *args, **kwargs):
         super(Post, self), __init__(*args, **kwargs)
-        self.slug = generate_slug()
+        self.generate_slug()
 
     def generate_slug(self):
         if self.title:
--- view.py
+++ view.py
@@ -20,3 +20,17 @@ def log():
 #@app.route('/admin')
 #def admin():
  #   return render_template('AdminPage.html')
+
+@app.route('/test')
+def test():
+    POST_USERNAME="python"
+    POST_PASSWORD="python"
+
+    Session = sessionmaker(bind=engine)
+    s=Session()
+    query=s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]))
+    result=query.first()
+    if result:
+        return "Object found"
+    else:
+        return "Object not found" + POST_USERNAME + " " + POST_PASSWORD
